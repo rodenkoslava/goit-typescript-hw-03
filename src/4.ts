147d982108c36d51f76633ec86a17ad29774edc8
rodenkoslava <131722989+rodenkoslava@users.noreply.github.com>
2023-12-27 20:10:21 +0100

    commit
    commit

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,36 @@
+class Key {
+  private signature: number = Math.random();
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+  constructor(protected key: Key) {}
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    this.door ? this.tenants.push(person) : console.log("Двері зачинені");
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    this.door = key.getSignature() === this.key.getSignature();
+    console.log(this.door ? "Двері відчинені" : "Не той ключ. Двері зачинені");
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +39,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
